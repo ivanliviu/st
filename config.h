0ba964c97b3e0543144a2daa35ff53a967b72f1c
Liviu Ivan <liviuivan@protonmail.com>
2024-09-29 13:44:52 +0300

    change theme to dracula

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "CaskaydiaCove Nerd Font Mono:pixelsize=32:antialias=true:autohint=true";
+static char *font2[] = { "Symbols Nerd Font Mono:pixelsize=32:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
@@ -104,39 +104,42 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.875;
 float alphaOffset = 0.0;
 float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
-	[255] = 0,
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
-	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
-};
+    /* 8 normal colors */
+    [8]  = "#080020", /* black   */
+    [9]  = "#ff8080", /* red     */
+    [10] = "#80ff80", /* green   */
+    [11] = "#ffff80", /* yellow  */
+    [12] = "#a080ff", /* blue    */
+    [13] = "#ff80c0", /* magenta */
+    [14] = "#80ffff", /* cyan    */
+    [15] = "#e7dfff", /* white   */
 
+    /* 8 bright colors */
+    [0] = "#1e1c24", /* black   */
+    [1] = "#ffc0c0", /* red     */
+    [2] = "#c0ffc0", /* green   */
+    [3] = "#ffffc0", /* yellow  */
+    [4] = "#d0c0ff", /* blue    */
+    [5] = "#ffc0e0", /* magenta */
+    [6] = "#c0ffff", /* cyan    */
+    [7] = "#ffffff", /* white   */
+
+    [255] = 0,
+    /* more colors can be added after 255 to use with DefaultXX */
+    [256] = "#edfbc3", /* cursor */
+    [257] = "#ff0000", /* rev cursor */
+    [258] = "#1e1c24", /* background */
+    [259] = "#edfbc3", /* foreground */
+};
 
 /*
  * Default colors (colorname index)
@@ -554,4 +557,3 @@ static char ascii_printable[] =
 	" !\"#$%&'()*+,-./0123456789:;<=>?"
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
-
